0a6ace56534e6e8c66d43a4234aad9c3da07c36a
seongsoo <seongsoo@localhost>
2020-11-12 19:24:55 +0900

    if문 활

--- Gugudan/src/Gugudan.java
+++ Gugudan/src/Gugudan.java
@@ -2,21 +2,38 @@ import java.util.Scanner;
 
 public class Gugudan {
 	public static void main(String[] args) {
-		//6단
-		System.out.println("6단");
-		int i = 1;
-		while (i < 10) {
-			// 곱셈 결 출력
-			System.out.println(6 * i);
-			// i 값을 1 증가
-			i = i + 1; // i++
-		}
+		// 8, 9단
+		System.out.println("구구단 중 출력할 단은? : ");
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+        System.out.println("사용자가 입력한 값 : " + number);
+        
+        if (number < 2) {
+        	System.out.println("값을 잘못 입력했습니다.");
+        }	else if (number > 9) {
+        	System.out.println("값을 잘못 입력했습니다.");
+        }	else {
+        		for(int i = 1; i < 10; i++) {
+        			System.out.println(number * i);
+        		}
+        }
 		
-		//7단
-		System.out.println("7단");
-		for(int j = 1; j < 10; j++) {
-			System.out.println(7 * j);
-		}
+		
+//		//6단
+//		System.out.println("6단");
+//		int i = 1;
+//		while (i < 10) {
+//			// 곱셈 결 출력
+//			System.out.println(6 * i);
+//			// i 값을 1 증가
+//			i = i + 1; // i++
+//		}
+//		
+//		//7단
+//		System.out.println("7단");
+//		for(int j = 1; j < 10; j++) {
+//			System.out.println(7 * j);
+//		}
 		
 		
 		// 2단
