1460107001835787eca32b9e30b741f05838b52c
seongsoo <seongsoo@172.30.1.5>
2020-11-13 15:42:33 +0900

    이중 for문 활용

--- Gugudan/src/Gugudan.java
+++ Gugudan/src/Gugudan.java
@@ -1,97 +1,44 @@
-import java.util.Scanner;
-
 public class Gugudan {
 	public static void main(String[] args) {
-		// 8, 9단
-		System.out.println("구구단 중 출력할 단은? : ");
-        Scanner scanner = new Scanner(System.in);
-        int number = scanner.nextInt();
-        System.out.println("사용자가 입력한 값 : " + number);
-        
-        if (number < 2) {
-        	System.out.println("값을 잘못 입력했습니다.");
-        }	else if (number > 9) {
-        	System.out.println("값을 잘못 입력했습니다.");
-        }	else {
-        		for(int i = 1; i < 10; i++) {
-        			System.out.println(number * i);
-        		}
-        }
 		
+		// 2단부터 시작하고 9단까지 실행하며 반복문이 한번 돌때마다 1씩 증가합니다
+		for(int i=2; i < 10; i++) {
+			System.out.println(i + "단을 출력합니다.");
+			
+			// # 위에서 i의 값이 2라면 2x1 ~ 2x9 까지 진행 합니다.
+	        // # 이 반복문(j)이 종료되면 위의 반복문 변수 i가 1 증가하여
+	        // 다시 3x1 ~ 3x9 까지 진행하는 식으로 9단까지 반복 합니다
+			for(int j=0; j < 10; j++) {
+				System.out.println(i + " x " + (j+1) + " = " + i * (j+1));
+			}
+			System.out.println();
+		}
+	}
+}	
 		
-//		//6단
-//		System.out.println("6단");
-//		int i = 1;
-//		while (i < 10) {
-//			// 곱셈 결 출력
-//			System.out.println(6 * i);
-//			// i 값을 1 증가
-//			i = i + 1; // i++
+//		for(int i=0; i < result.length; i++) {
+//			result[i] = 2 * (i + 1);
 //		}
 //		
-//		//7단
-//		System.out.println("7단");
-//		for(int j = 1; j < 10; j++) {
-//			System.out.println(7 * j);
+//		for(int i=0; i < result.length; i++) {
+//			System.out.println(result[i]);
+//		}
+//		
+//		int[] times3 = new int[9];
+//		for(int i=0; i < times3.length; i++) {
+//			times3[i] = 3 * (i + 1);
+//		}
+//		
+//		for(int i=0; i < times3.length; i++) {
+//			System.out.println(times3[i]);
+//		}
+//		
+//		int[] times4 = new int[9];
+//		for(int i=0; i < times4.length; i++) {
+//			times4[i] = 4 * (i + 1);
+//		}
+//		
+//		for(int i=0; i < times4.length; i++) {
+//			System.out.println(times4[i]);
 //		}
 		
-		
-		// 2단
-//		System.out.println("2단");
-//		System.out.println(2 * 1);
-//		System.out.println(2 * 2);
-//		System.out.println(2 * 3);
-//		System.out.println(2 * 4);
-//		System.out.println(2 * 5);
-//		System.out.println(2 * 6);
-//		System.out.println(2 * 7);
-//		System.out.println(2 * 8);
-//		System.out.println(2 * 9);
-		
-		// 3단
-//		System.out.println("3단");
-//		System.out.println(3 * 1);
-//		System.out.println(3 * 2);
-//		System.out.println(3 * 3);
-//		System.out.println(3 * 4);
-//		System.out.println(3 * 5);
-//		System.out.println(3 * 6);
-//		System.out.println(3 * 7);
-//		System.out.println(3 * 8);
-//		System.out.println(3 * 9);
-		
-		// 4단
-//		System.out.println("4단");
-//		int result = 4 * 1;
-//		System.out.println(result);
-//		result = 4 * 2;
-//		System.out.println(result);
-//		result = 4 * 3;
-//		System.out.println(result);
-//		result = 4 * 4;
-//		System.out.println(result);
-//		result = 4 * 5;
-//		System.out.println(result);
-//		result = 4 * 6;
-//		System.out.println(result);
-//		result = 4 * 7;
-//		System.out.println(result);
-//		result = 4 * 8;
-//		System.out.println(result);
-//		result = 4 * 9;
-//		System.out.println(result);
-		
-//		System.out.println("구구단 중 출력할 단은? : ");
-//		Scanner scanner = new Scanner(System.in);
-//        int number = scanner.nextInt();
-//        System.out.println(number * 1);
-//        System.out.println(number * 2);
-//        System.out.println(number * 3);
-//        System.out.println(number * 4);
-//        System.out.println(number * 5);
-//        System.out.println(number * 6);
-//        System.out.println(number * 7);
-//        System.out.println(number * 8);
-//        System.out.println(number * 9);
-	}
-}
