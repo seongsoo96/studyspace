7a86fdeca2e4d566939fdc4dfd3edafe0398d5a7
seongsoo <seongsoo@localhost>
2020-11-19 15:59:38 +0900

    Update calendar main()

--- src/ssl/calendar/Calendar.java
+++ src/ssl/calendar/Calendar.java
@@ -3,8 +3,12 @@ package ssl.calendar;
 public class Calendar {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("일 월 화 수 목 금 토");
+		System.out.println("--------------------");
+		System.out.println(" 1  2  3  4  5  6  7");
+		System.out.println(" 8  9 10 11 12 13 14");
+		System.out.println("15 16 17 18 19 20 21");
+		System.out.println("22 23 24 25 26 27 28");
 	}
 
 }
