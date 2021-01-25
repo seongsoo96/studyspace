296c32301a358152c09f56f0f405aff22fb9665f
seongsooL <seongsoo.dev@gmail.com>
2021-01-25 23:03:22 +0900

    last

--- src/git/Test.java
+++ src/git/Test.java
@@ -3,6 +3,6 @@ package git;
 public class Test {
 	public static void main(String[] args) {
 		System.out.println("Hello git commit");
-		System.out.println("change passwor");
+		System.out.println("change passw");
 	}
 }
