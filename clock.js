35a4ddb769f225d6943b7ab321f6a35518424136
seongsooL <seongsoo.dev@gmail.com>
2021-02-25 23:00:23 +0900

    clock in momentum

--- clock.js
+++ clock.js
@@ -1,22 +1,20 @@
-const title = document.querySelector("#title");
+const clockContainer = document.querySelector(".js-clock"),
+   clockTitle = clockContainer.querySelector("h1");
 
-const CLICKED_CLASS = "clicked";
-/*
-function handleClick() {
-    const hasClass = title.classList.contains(CLICKED_CLASS)
-    if(!hasClass){
-        title.classList.add(CLICKED_CLASS);
-    } else {
-        title.classList.remove(CLICKED_CLASS);
-    }
-}*/
-
-function handleClick() {
-    title.classList.toggle(CLICKED_CLASS);
+function getTime(){
+    const date = new Date();
+    const minutes = date.getMinutes();
+    const hours = date.getHours();
+    const seconds = date.getSeconds();
+    clockTitle.innerText = `${hours < 10 ? `0${hours}` : hours
+    }:${minutes < 10 ? `0${minutes}` : minutes
+    }:${seconds < 10 ? `0${seconds}` : seconds
+    }`;
 }
 
 function init(){
-    title.addEventListener("click", handleClick);
+    getTime();
+    setInterval(getTime, 1000);
 }
 init();
 
