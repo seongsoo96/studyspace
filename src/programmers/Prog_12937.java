d0a4692e68bb795c5b8344ee4fc002e2523e9fed
seongsooL <seongsoo.dev@gmail.com>
2021-03-02 16:32:03 +0900

    [프로그래머스] 12937번 - 짝수와 홀수

---
+++ src/programmers/Prog_12937.java
@@ -0,0 +1,23 @@
+package programmers;
+
+public class Prog_12937 {
+	
+	public static String solution(int num) {
+		String answer = "";
+		
+		if((num%2 == 0) || (num == 0)){	// num을 2로 나눈 나머지가 0 or num이 0이면
+			answer = "Even";			// 짝수
+		} else {						// 그 외는
+			answer = "Odd";				// 홀수
+		}
+		
+		return answer;
+	}
+	
+	public static void main(String[] args) {
+		for(int i=0; i<100; i++) {
+			System.out.println(solution(i));
+		}
+	}
+	
+}
