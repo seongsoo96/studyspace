8c4983edf01360fd26c54e057c2d45a6e3c235d7
seongsooL <seongsoo.dev@gmail.com>
2021-03-02 16:58:43 +0900

    [프로그래머스] 12944번 - 평균 구하기

---
+++ src/programmers/Prog_12944.java
@@ -0,0 +1,31 @@
+package programmers;
+
+public class Prog_12944 {
+
+	public static double solution(int[] arr) {
+		double answer = 0;
+		int sum = 0;
+
+		for(int i=0; i<arr.length; i++){
+			// 모든 배열 원소들의 합
+			sum += arr[i];
+
+			// 평균
+			answer = sum / (double)arr.length;
+		}
+
+		return answer;
+	}
+	
+	public static void main(String[] args) {
+		int[] arr;
+		arr = new int[] {1, 2, 3, 4};
+		System.out.println(solution(arr));
+		arr = new int[] {5, 5};
+		System.out.println(solution(arr));
+		arr = new int[] {3, 5, 6, 12};
+		System.out.println(solution(arr));
+		
+	}
+
+}
