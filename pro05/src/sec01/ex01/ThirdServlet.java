f8cc167c22fd21c155349d0e25666a397c813377
seongsooL <seongsoo.dev@gmail.com>
2021-03-06 10:28:57 +0900

    서블릿 이해하기 understanding how works Servlets

---
+++ pro05/src/sec01/ex01/ThirdServlet.java
@@ -0,0 +1,45 @@
+package sec01.ex01;
+
+import java.io.IOException;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ThirdServlet
+ */
+@WebServlet("/third")
+public class ThirdServlet extends HttpServlet {
+
+	/**
+	 * @see Servlet#init(ServletConfig)
+	 */
+	public void init(ServletConfig config) throws ServletException {
+		System.out.println("ThirdServlet init 메서드 호출");
+	}
+
+	/**
+	 * @see Servlet#destroy()
+	 */
+	public void destroy() {
+		System.out.println("ThirdServlet destroy 메서드 호출");
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("ThirdServlet doGet 메서드 호출");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("ThirdServlet doPost 메서드 호출");
+	}
+
+}
