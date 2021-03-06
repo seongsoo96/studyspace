39d1442d03ac717a0c5734bb98f8e3434d050d6e
seongsooL <seongsoo.dev@gmail.com>
2021-03-06 10:32:55 +0900

    서블릿 기초

---
+++ pro06/src/sec01/ex01/LoginServlet.java
@@ -0,0 +1,42 @@
+package sec01.ex01;
+
+import java.io.IOException;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+@WebServlet("/login")
+public class LoginServlet extends HttpServlet {
+       
+	/**
+	 * @see Servlet#init(ServletConfig)
+	 */
+	public void init(ServletConfig config) throws ServletException {
+		System.out.println("init 메소드 호출");
+	}
+
+	/**
+	 * @see Servlet#destroy()
+	 */
+	public void destroy() {
+		System.out.println("destroy 메소드 호출");
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("utf-8");
+		String user_id = request.getParameter("user_id");
+		String user_pw = request.getParameter("user_pw");
+		System.out.println("아이디: " + user_id);
+		System.out.println("비밀번호: " + user_pw);
+	}
+
+}
---
+++ pro06/src/sec02/ex01/LoginServlet2.java
@@ -0,0 +1,53 @@
+package sec02.ex01;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.Servlet;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class LoginServlet2
+ */
+@WebServlet("/login2")
+public class LoginServlet2 extends HttpServlet {
+	/**
+	 * @see Servlet#init(ServletConfig)
+	 */
+	public void init(ServletConfig config) throws ServletException {
+		System.out.println("init 메서드 호출");
+	}
+
+	/**
+	 * @see Servlet#destroy()
+	 */
+	public void destroy() {
+		System.out.println("destroy 메서드 호출");
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
+		response.setContentType("text/html;charset=utf-8");
+		PrintWriter out = response.getWriter();
+		String id = request.getParameter("user_id");
+		String pw = request.getParameter("user_pw");
+		
+		String data = "<html>";
+		data += "<body>";
+		data += "아이디 : " + id;
+		data += "<br>";
+		data += "패스워드 : " + pw;
+		data += "</body>";
+		data += "</html>";
+		out.print(data);
+	}
+
+}
