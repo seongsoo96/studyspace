c4f0341d88bdfb9aef8d5c3b75b473d0cd892f37
seongsooL <seongsoo.dev@gmail.com>
2021-03-07 10:55:17 +0900

    PreparedStatement를 이용한 회원 정보 실습

---
+++ pro07/src/sec01/ex02/MemberDAO.java
@@ -0,0 +1,64 @@
+package sec01.ex02;
+
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+import sec01.ex02.MemberVO;
+
+public class MemberDAO {
+	private static final String driver = "oracle.jdbc.driver.OracleDriver";
+	private static final String url = "jdbc:oracle:thin:@localhost:1521:xe";
+	private static final String user = "scott";
+	private static final String pwd = "tiger";
+	private Connection con;
+	private PreparedStatement pstmt;
+	
+	public List<MemberVO> listMembers(){
+		List<MemberVO> list = new ArrayList<MemberVO>();
+		try {
+			connDB();
+			String query = "select * from t_member ";
+			System.out.println("preparesStatement: " + query);
+			pstmt = con.prepareStatement(query);
+			ResultSet rs = pstmt.executeQuery();
+			
+			while(rs.next()) {
+				String id = rs.getString("id");
+				String pwd = rs.getString("pwd");
+				String name = rs.getString("name");
+				String email = rs.getString("email");
+				Date joinDate = rs.getDate("joinDate");
+				
+				MemberVO vo = new MemberVO();
+				vo.setId(id);
+				vo.setPwd(pwd);
+				vo.setName(name);
+				vo.setEmail(email);
+				vo.setJoinDate(joinDate);
+				list.add(vo);
+			} 
+			rs.close();
+			pstmt.close();
+			con.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return list;
+	}
+	
+	private void connDB() {
+		try {
+			Class.forName(driver);
+			System.out.println("Oracle 드라이버 로딩 성공");
+			con = DriverManager.getConnection(url, user, pwd);
+			System.out.println("Connection 생성 성공");
+		} catch(Exception e) {
+			e.printStackTrace(); 
+		}
+	}
+}
