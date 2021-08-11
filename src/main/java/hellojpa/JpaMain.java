9958cd32178da876c19a1947d45abff0b796aae0
seongsoo96 <seongsoo.dev@gmail.com>
2021-08-12 00:56:53 +0900

    영속성 컨텍스트

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -17,15 +17,49 @@ public class JpaMain {
         tx.begin();
 
         try {
-//            Member findMember = em.find(Member.class, 1L);
-            List<Member> result = em.createQuery("select m from Member m", Member.class)
-                    .setFirstResult(1)
-                    .setMaxResults(10)
-                    .getResultList();
+/*
+            //비영속
+            Member member = new Member();
+            member.setId(101L);
+            member.setName("HelloJPA");
 
-            for(Member member : result) {
-                System.out.println("member.getName() = " + member.getName());
-            }
+            //영속
+            System.out.println("=== BEFORE ===");
+            em.persist(member);
+            System.out.println("=== AFTER ===");
+
+            //1차 캐시에서 조회(select 쿼리문이 안나감)
+            Member findMember = em.find(Member.class, 101L);
+
+            System.out.println("findMember.id = " + findMember.getId());
+            System.out.println("findMember.name = " + findMember.getName());
+
+            //영속 엔티티의 동일성 보장
+            Member findMember1 = em.find(Member.class, 101L);
+            Member findMember2 = em.find(Member.class, 101L);
+
+            System.out.println("result = " + (findMember1 == findMember2));
+*/
+
+            //엔티티 등록
+            Member member1 = new Member(150L, "A");
+            Member member2 = new Member(160L, "B");
+
+            em.persist(member1);
+            em.persist(member2);
+            System.out.println("=========================");
+
+            //엔티티 수정 (변경 감지)
+            Member member = em.find(Member.class, 150L);
+            member.setName("ZZZZ");
+//            em.persist(member);  <- 틀림
+            System.out.println("=========================");
+
+            //엔티티 삭제
+            Member findMember = em.find(Member.class, 160L);
+            em.remove(findMember);
+            System.out.println("=========================");
+
 
             tx.commit();
         } catch (Exception e) {
