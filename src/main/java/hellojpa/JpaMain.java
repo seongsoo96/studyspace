5e099e3e978d462d00baa543ef2bc76bd7bdc26e
seongsoo96 <seongsoo.dev@gmail.com>
2021-08-17 22:08:48 +0900

    필드와 컬럼매핑

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -17,58 +17,6 @@ public class JpaMain {
         tx.begin();
 
         try {
-/*
-            //비영속
-            Member member = new Member();
-            member.setId(101L);
-            member.setName("HelloJPA");
-
-            //영속
-            System.out.println("=== BEFORE ===");
-            em.persist(member);
-            System.out.println("=== AFTER ===");
-
-            //1차 캐시에서 조회(select 쿼리문이 안나감)
-            Member findMember = em.find(Member.class, 101L);
-
-            System.out.println("findMember.id = " + findMember.getId());
-            System.out.println("findMember.name = " + findMember.getName());
-
-            //영속 엔티티의 동일성 보장
-            Member findMember1 = em.find(Member.class, 101L);
-            Member findMember2 = em.find(Member.class, 101L);
-
-            System.out.println("result = " + (findMember1 == findMember2));
-*/
-
-
-/*
-            //플러시
-            //  영속성 컨텍스트를 플러하는 방법
-            //  em.flush() - 직접 호출
-            //  트랜잭션 커밋 - 플러시 자동 호출
-            //  JPQL쿼리 실행 - 플러시 자동 호출
-            //  플러시는
-            //  영속성 컨텍스트를 비우지 않음
-            //  영속성 컨텍스트의 변경내용을 데이터베이스에 동기화
-            //  트랜잭션이라는 작업 단위가 중요! -> 커밋 직전에만 동기화 하면 됨.
-            Member member200 = new Member(200L, "member200");
-            em.persist(member200);
-
-            em.flush();
-            System.out.println("=========================");
-*/
-
-            //준영속
-            Member member = em.find(Member.class, 150L);
-            member.setName("AAAAA");
-
-//            em.detach(member);
-            em.clear();
-
-            Member member2 = em.find(Member.class, 150L);
-
-            System.out.println("=========================");
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -1,36 +1,38 @@
 package hellojpa;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Date;
 
 @Entity
 public class Member {
 
     @Id
     private Long id;
-    private String name;
+
+    @Column(name = "name", nullable = false)
+    private String username;
+
+    private Integer age;
+
+    @Enumerated(EnumType.STRING) //ORDINAL 사용 금지!
+    private RoleType roleType;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createdDate;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date lastModifiedDate;
+
+    @Lob
+    private String description;
+
+    @Transient //매핑 무시
+    private int temp;
 
     public Member() {
     }
 
-    public Member(Long id, String name) {
-        this.id = id;
-        this.name = name;
-    }
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 }
