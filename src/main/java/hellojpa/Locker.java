b2ca0153d4c7d71c99f7bd32b4cf0aa783d3be7e
seongsoo96 <seongsoo.dev@gmail.com>
2021-08-21 16:34:40 +0900

    일대일 매핑

---
+++ src/main/java/hellojpa/Locker.java
@@ -0,0 +1,15 @@
+package hellojpa;
+
+import javax.persistence.*;
+
+@Entity
+public class Locker {
+
+    @Id @GeneratedValue
+    private Long id;
+
+    private String name;
+
+    @OneToOne(mappedBy = "locker")
+    private Member member;
+}
