b046f43b263055e7cb333e6cdf62035cb3df5deb
seongsoo96 <seongsoo.dev@gmail.com>
2021-08-22 18:24:35 +0900

    상속관계 매핑

---
+++ src/main/java/hellojpa/Book.java
@@ -0,0 +1,10 @@
+package hellojpa;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Book extends Item {
+
+    private String author;
+    private String isbn;
+}
--- src/main/java/hellojpa/Item.java
+++ src/main/java/hellojpa/Item.java
@@ -1,22 +1,16 @@
 package hellojpa;
 
 import javax.persistence.*;
-import java.util.ArrayList;
-import java.util.List;
 
 @Entity
-public class Product {
+@Inheritance(strategy = InheritanceType.JOINED)
+public class Item {
 
     @Id @GeneratedValue
     private Long id;
 
     private String name;
-
-//    @ManyToMany(mappedBy = "products")
-//    private List<Member> members = new ArrayList<>();
-
-    @OneToMany(mappedBy = "product")
-    private List<MemberProduct> members = new ArrayList<>();
+    private int price;
 
     public Long getId() {
         return id;
@@ -32,5 +26,13 @@ public class Product {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
     }
 }
