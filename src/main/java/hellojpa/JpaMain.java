995e121b761fc336cc527ec90b2b9f874900341d
seongsoo96 <seongsoo.dev@gmail.com>
2021-08-30 22:48:00 +0900

    Criteria은 실무 사용 비추

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -4,6 +4,9 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
 import java.util.List;
 import java.util.Set;
 
@@ -19,52 +22,20 @@ public class JpaMain {
 
         try {
 
-            //값 타입 저장
-            Member member = new Member();
-            member.setUsername("member1");
-            member.setHomeAddress(new Address("homeCity", "street", "10000"));
+            //Criteria 사용 준비
+            CriteriaBuilder cb = em.getCriteriaBuilder();
+            CriteriaQuery<Member> query = cb.createQuery(Member.class);
 
-            member.getFavoriteFoods().add("치킨");
-            member.getFavoriteFoods().add("피자");
-            member.getFavoriteFoods().add("족발");
+            Root<Member> m = query.from(Member.class);
 
-            member.getAddressHistory().add(new Address("old1", "street", "10000"));
-            member.getAddressHistory().add(new Address("old2", "street", "10000"));
+            CriteriaQuery cq = query.select(m);
 
-            em.persist(member);
-
-            em.flush();
-            em.clear();
-
-            System.out.println("=========== START ============");
-            Member findMember = em.find(Member.class, member.getId());
-
-/*
-            //값 타입 조회
-            List<Address> addressHistory = findMember.getAddressHistory();
-            for(Address address : addressHistory) {
-                System.out.println("address = " + address.getCity());
+            String username = "hello";
+            if(username != null) {
+                cq = query.select(m).where(cb.equal(m.get("username"), "kim"));
             }
-            Set<String> favoriteFoods = findMember.getFavoriteFoods();
-            for(String favoriteFood : favoriteFoods) {
-                System.out.println("favoriteFood = " + favoriteFood);
-            }
-*/
 
-            //값 타입 수정
-            //homeCity -> newCity
-//            findMember.getHomeAddress().setCity("newCity"); <- 안 좋은 방법.
-            Address a = findMember.getHomeAddress();
-            findMember.setHomeAddress(new Address("newCity", a.getStreet(), a.getZipcode()));
-
-            //치킨 -> 한식
-            findMember.getFavoriteFoods().remove("치킨");
-            findMember.getFavoriteFoods().add("한식");
-
-            findMember.getAddressHistory().remove(new Address("old1", "street", "10000"));
-            findMember.getAddressHistory().add(new Address("newCity1", "street", "10000"));
-
-            //값 타입 컬렉션에 변경 사항이 발생하면, 주인 엔티티와 연관된 모든 데이터를 삭제하고, 값 타입 컬렉션에 있는 현재 값을 모두 다시 저장한다.
+            List resultList = em.createQuery(cq).getResultList();
 
             tx.commit();
         } catch (Exception e) {
