3d2d0700f1683c4bc267958551124524e577480f
seongsoo96 <seongsoo.dev@gmail.com>
2021-10-04 21:36:50 +0900

    조인

--- jpa-jpql/src/main/java/jpql/JpaMain.java
+++ jpa-jpql/src/main/java/jpql/JpaMain.java
@@ -14,26 +14,24 @@ public class JpaMain {
 
         try {
 
-            for(int i=0; i<100; i++) {
-                Member member = new Member();
-                member.setUsername("member" + i);
-                member.setAge(i);
-                em.persist(member);
-            }
+            Team team = new Team();
+            team.setName("teamA");
+            em.persist(team);
+
+            Member member = new Member();
+            member.setUsername("member");
+            member.setAge(10);
+
+            member.setTeam(team);
+
+            em.persist(member);
 
             em.flush();
             em.clear();
 
-            //페이징
-            List<Member> result = em.createQuery("select m from Member m order by m.age desc", Member.class)
-                    .setFirstResult(1)
-                    .setMaxResults(10)
+            String query = "select m from Member m inner join m.team t";
+            List<Member> result = em.createQuery(query, Member.class)
                     .getResultList();
-
-            System.out.println("result.size() = " + result.size());
-            for(Member member1 : result) {
-                System.out.println("member1 = " + member1);
-            }
 
             tx.commit();
         } catch (Exception e) {
