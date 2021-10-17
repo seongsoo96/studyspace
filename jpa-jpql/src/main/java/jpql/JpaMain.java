c5425f0a76f2e5ebe7bcfbbc22fe2a843ea8528d
seongsoo96 <seongsoo.dev@gmail.com>
2021-10-17 15:08:29 +0900

    기본 CASE 식

--- jpa-jpql/src/main/java/jpql/JpaMain.java
+++ jpa-jpql/src/main/java/jpql/JpaMain.java
@@ -30,18 +30,15 @@ public class JpaMain {
             em.flush();
             em.clear();
 
-            String query = "select m.username, 'HELLO', true from Member m " +
-                            "where m.type = :userType";
-
-            List<Object[]> result = em.createQuery(query)
-                    .setParameter("userType", MemberType.ADMIN)
+            String query =
+                    "select " +
+                            "case when m.age <= 10 then '학생요금' " +
+                            "     when m.age >= 60 then '경로요금' " +
+                            "     else '일반요금' " +
+                            "end " +
+                    "from Member m";
+            List<String> result = em.createQuery(query, String.class)
                     .getResultList();
-
-            for(Object[] objects : result) {
-                System.out.println("objects = " + objects[0]);
-                System.out.println("objects = " + objects[1]);
-                System.out.println("objects = " + objects[2]);
-            }
 
             tx.commit();
         } catch (Exception e) {
