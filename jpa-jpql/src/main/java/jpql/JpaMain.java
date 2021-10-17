214fe6521c3b0a04df12749b916722840e7dee46
seongsoo96 <seongsoo.dev@gmail.com>
2021-10-17 15:16:52 +0900

    coalesce 하나씩 조회해서 null이 아니면 반환

--- jpa-jpql/src/main/java/jpql/JpaMain.java
+++ jpa-jpql/src/main/java/jpql/JpaMain.java
@@ -19,7 +19,7 @@ public class JpaMain {
             em.persist(team);
 
             Member member = new Member();
-            member.setUsername("member");
+            member.setUsername(null);
             member.setAge(10);
             member.setType(MemberType.ADMIN);
 
@@ -30,15 +30,14 @@ public class JpaMain {
             em.flush();
             em.clear();
 
-            String query =
-                    "select " +
-                            "case when m.age <= 10 then '학생요금' " +
-                            "     when m.age >= 60 then '경로요금' " +
-                            "     else '일반요금' " +
-                            "end " +
-                    "from Member m";
+            String query = "select coalesce(m.username, '이름 없는 회원') as username " +
+                    "from Member m ";
             List<String> result = em.createQuery(query, String.class)
                     .getResultList();
+
+            for(String s : result) {
+                System.out.println("s = " + s);
+            }
 
             tx.commit();
         } catch (Exception e) {
