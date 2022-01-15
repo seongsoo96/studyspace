34b66046d6c45fd3c1020f7d55b37fffcb1afbeb
seongsoo96 <seongsoo.dev@gmail.com>
2022-01-15 23:15:12 +0900

    react 기본 - 웹팩 설정 완료

--- react-webgame/lecture/WordRelay.jsx
+++ react-webgame/lecture/WordRelay.jsx
@@ -3,11 +3,11 @@ const { Component } = React;
 
 class WordRelay extends Component {
     state = {
-
+        text: 'Hello, webpack',
     };
 
     render() {
-
+        return <h1>{this.state.text}</h1>;
     };
 }
 
--- react-webgame/lecture/webpack.config.js
+++ react-webgame/lecture/webpack.config.js
@@ -12,6 +12,16 @@ module.exports = {
         app: ['./client'], // 이미 클라이언트가 워드릴레이를 불러옴 -> 생략가능
     }, // 입력
 
+    module: {
+        rules: [{
+            test: /\.jsx?/,
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env', '@babel/preset-react'],
+            },
+        }]
+    },
+
     output: {
         path: path.join(__dirname, 'dist'),
         filename: 'app.js'
