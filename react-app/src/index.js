10c7973154c603fd34716140213b9982b1ad1b52
seongsoo96 <seongsoo.dev@gmail.com>
2022-02-06 22:12:04 +0900

    add TodoList in index.js

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import TodoList from './TodoList';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <TodoList />
   </React.StrictMode>,
   document.getElementById('root')
 );
