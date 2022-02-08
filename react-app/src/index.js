024296b165665e1a7111383e5ec39ef97ad656e5
seongsoo96 <seongsoo.dev@gmail.com>
2022-02-08 23:17:09 +0900

    add CoinTracker in index.js

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -1,12 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import TodoList from './TodoList';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import TodoList from "./TodoList";
+import CoinTracker from "./CoinTracker";
 
 ReactDOM.render(
-  <React.StrictMode>
-    {/* <App /> */}
-    <TodoList />
-  </React.StrictMode>,
-  document.getElementById('root')
+	<React.StrictMode>
+		{/* <App /> */}
+		{/* <TodoList /> */}
+		<CoinTracker />
+	</React.StrictMode>,
+	document.getElementById("root")
 );
