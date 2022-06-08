103dd7c4175bc90595a9af13b92961fb7cc9c70f
seongsoo96 <seongsoo.dev@gmail.com>
2022-06-09 00:19:49 +0900

    about useEffect

--- repetition/cra-beginners/src/App.js
+++ repetition/cra-beginners/src/App.js
@@ -1,11 +1,32 @@
-import Button from "./Button";
-import styles from "./App.module.css";
+import { useState, useEffect } from "react";
 
 function App() {
+	const [counter, setCounter] = useState(0);
+	const [keyword, setKeyword] = useState("");
+	const onClick = () => setCounter((prev) => prev + 1);
+	const onChange = (event) => setKeyword(event.target.value);
+	useEffect(() => {
+		console.log("I run only once.");
+	}, []);
+	useEffect(() => {
+		console.log("I run when 'keyword' changes");
+	}, [keyword]);
+	useEffect(() => {
+		console.log("I run when 'counter' changes");
+	}, [counter]);
+	useEffect(() => {
+		console.log("I run when 'keyword & counter' changes");
+	}, [keyword, counter]);
 	return (
 		<div>
-			<h1 className={styles.title}>CRA start!</h1>
-			<Button text={"Continue"} />
+			<input
+				value={keyword}
+				onChange={onChange}
+				type="text"
+				placeholder="Search here..."
+			/>
+			<h1>{counter}</h1>
+			<button onClick={onClick}>click me</button>
 		</div>
 	);
 }
