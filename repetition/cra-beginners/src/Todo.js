aa0fc2255df7b0526681f465d396be91185ce674
seongsoo96 <gasmathdog@gmail.com>
2022-06-29 20:25:33 +0900

    revise to use useState coding Todo.js

---
+++ repetition/cra-beginners/src/Todo.js
@@ -0,0 +1,37 @@
+import { useState, useEffect } from 'react';
+
+function Todo() {
+  const [toDo, setToDo] = useState('');
+  const [toDos, setToDos] = useState([]);
+  const onChange = (event) => setToDo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (toDo === '') {
+      return;
+    }
+    setToDos((currentArray) => [toDo, ...currentArray]);
+    setToDo('');
+  };
+  return (
+    <div>
+      <h1>My To Dos ({toDos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          value={toDo}
+          type="text"
+          placeholder="Write your to do..."
+        />
+        <button>Add To Do</button>
+      </form>
+      <hr />
+      <ul>
+        {toDos.map((toDo, index) => (
+          <li key={index}>{toDo}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default Todo;
--- repetition/cra-beginners/src/index.js
+++ repetition/cra-beginners/src/index.js
@@ -1,14 +1,14 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import UseEffect from "./UseEffect";
-import Cleanup from "./Cleanup";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Todo from './Todo';
+import UseEffect from './UseEffect';
+import Cleanup from './Cleanup';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-	// <React.StrictMode>
-	<>
-		<UseEffect />
-		<Cleanup />
-	</>
-	// </React.StrictMode>
+  // <React.StrictMode>
+  <>
+    <Todo />
+  </>
+  // </React.StrictMode>
 );
