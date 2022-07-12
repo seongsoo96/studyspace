5cb5cee78c68e27d1483c95fb628f0cf3527633c
seongsoo96 <gasmathdog@gmail.com>
2022-07-12 20:20:27 +0900

    make the coinTracker using api

---
+++ repetition/cra-beginners/src/CoinTracker.js
@@ -0,0 +1,32 @@
+import { useEffect, useState } from 'react';
+
+function CoinTracker() {
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+  useEffect(() => {
+    fetch('http://api.coinpaprika.com/v1/tickers')
+      .then((res) => res.json())
+      .then((json) => {
+        setCoins(json);
+        setLoading(false);
+      });
+  }, []);
+  return (
+    <div>
+      <h1>The Coins! ({coins.length})</h1>
+      {loading ? (
+        <strong>Loading...</strong>
+      ) : (
+        <select>
+          {coins.map((coin) => (
+            <option key={coin.id}>
+              {coin.name} ({coin.symbol}): ${coin.quotes.USD.price} USD
+            </option>
+          ))}
+        </select>
+      )}
+    </div>
+  );
+}
+
+export default CoinTracker;
