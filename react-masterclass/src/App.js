b292af587e9fc78eb9ded91c13eb55828d04b34b
seongsoo96 <gasmathdog@gmail.com>
2022-07-16 13:11:06 +0900

    how to set Themes

--- react-masterclass/src/App.js
+++ react-masterclass/src/App.js
@@ -2,51 +2,21 @@ import styled, { keyframes } from 'styled-components';
 
 const Wrapper = styled.div`
   display: flex;
-`;
-
-const animation = keyframes`
-  0%{
-    transform:rotate(0deg);
-    border-radius:0px;
-  }
-  50%{
-    border-radius:100px;
-  }
-  100%{
-    transform:rotate(360deg);
-    border-radius:0px;
-  }
-`;
-
-const Emoji = styled.span`
-  font-size: 36px;
-`;
-
-const Box = styled.div`
-  display: flex;
+  width: 100vw;
+  height: 100vh;
   justify-content: center;
   align-items: center;
-  height: 200px;
-  width: 200px;
-  background-color: tomato;
-  animation: ${animation} 1s linear infinite;
-  ${Emoji} {
-    &:hover {
-      font-size: 72px;
-    }
-    &:active {
-      opacity: 0;
-    }
-  }
+  background-color: ${(props) => props.theme.backgroundColor};
+`;
+
+const Title = styled.h1`
+  color: ${(props) => props.theme.textColor};
 `;
 
 function App() {
   return (
     <Wrapper>
-      <Box>
-        <Emoji as="p">😆</Emoji>
-      </Box>
-      <Emoji>🔥</Emoji>
+      <Title>Hello</Title>
     </Wrapper>
   );
 }
--- react-masterclass/src/index.js
+++ react-masterclass/src/index.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { ThemeProvider } from 'styled-components';
+
+const darkTheme = {
+  textColor: 'whitesmoke',
+  backgroundColor: '#111',
+};
+
+const lightTheme = {
+  textColor: '#111',
+  backgroundColor: 'whitesmoke',
+};
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={darkTheme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
