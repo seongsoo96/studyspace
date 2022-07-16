0d09183e11a0848fbfddf5d3200e3b6786d74728
seongsoo96 <gasmathdog@gmail.com>
2022-07-16 11:28:41 +0900

    start to learn about styled-components

---
+++ react-masterclass/src/App.js
@@ -0,0 +1,26 @@
+import styled from 'styled-components';
+
+const Father = styled.div`
+  display: flex;
+`;
+
+const Box = styled.div`
+  background-color: ${(props) => props.bgColor};
+  width: 100px;
+  height: 100px;
+`;
+
+const Circle = styled(Box)`
+  border-radius: 50px;
+`;
+
+function App() {
+  return (
+    <Father>
+      <Box bgColor="teal" />
+      <Circle bgColor="gray" />
+    </Father>
+  );
+}
+
+export default App;
