ee07ddf2830dcd877b4ed1dac2ccd45977f9eb2f
seongsoo96 <gasmathdog@gmail.com>
2022-07-18 14:41:48 +0900

    how to use Props

---
+++ react-masterclass/src/Circle.tsx
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+const Container = styled.div<CircleProps>`
+  width: 200px;
+  height: 200px;
+  background-color: ${(props) => props.bgColor};
+  border-radius: 100px;
+`;
+
+interface CircleProps {
+  bgColor: string;
+}
+
+function Circle({ bgColor }: CircleProps) {
+  return <Container bgColor={bgColor} />;
+}
+
+export default Circle;
