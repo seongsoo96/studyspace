a31509300e7e5484dc0d56982712363c12406afb
seongsoo96 <gasmathdog@gmail.com>
2022-07-25 21:37:04 +0900

    컴포넌트 생명주기(Class)

--- react/fc-my-app/src/App.js
+++ react/fc-my-app/src/App.js
@@ -2,15 +2,17 @@ import './App.css';
 import Composition from './components/2-4.Props/Composition';
 import Extraction from './components/2-4.Props/Extraction/Extraction';
 import ClassComponent from './components/2-5.State/ClassComponent';
+import ClassComponent2 from './components/2-6.LifeCycle/ClassComponent';
 import FunctionalComponent from './components/2-5.State/FunctionalComponent';
 
 function App() {
   return (
     <div className="App">
-      <FunctionalComponent />
+      <ClassComponent2 />
+      {/* <FunctionalComponent />
       <ClassComponent />
       <Composition />
-      <Extraction />
+      <Extraction /> */}
     </div>
   );
 }
