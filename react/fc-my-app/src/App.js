a920d67fd6141154e5e6f1e15522b4a0f8e51c7e
seongsoo96 <gasmathdog@gmail.com>
2022-07-25 21:52:46 +0900

    이벤트(캡쳐, 버블링, e.preventDefault)

--- react/fc-my-app/src/App.js
+++ react/fc-my-app/src/App.js
@@ -4,11 +4,13 @@ import Extraction from './components/2-4.Props/Extraction/Extraction';
 import ClassComponent from './components/2-5.State/ClassComponent';
 import ClassComponent2 from './components/2-6.LifeCycle/ClassComponent';
 import FunctionalComponent from './components/2-5.State/FunctionalComponent';
+import Event from './components/2-7.Event/Event';
 
 function App() {
   return (
     <div className="App">
-      <ClassComponent2 />
+      <Event />
+      {/* <ClassComponent2 /> */}
       {/* <FunctionalComponent />
       <ClassComponent />
       <Composition />
