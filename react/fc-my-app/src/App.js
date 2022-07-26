7307140a99b4bdf60bbc15ca493b71d679bbf1f3
seongsoo96 <gasmathdog@gmail.com>
2022-07-26 20:39:23 +0900

    Hooks(useState, useReducer)

--- react/fc-my-app/src/App.js
+++ react/fc-my-app/src/App.js
@@ -9,20 +9,24 @@ import Condition from './components/2-8.ConditionalRendering/Condition';
 import List from './components/2-9.List/List';
 import ControlledComponent from './components/2-10.Form/ControlledComponent';
 import UncontrolledComponent from './components/2-10.Form/UncontrolledComponent';
+import State from './components/3-3.Hooks/State';
+import Reducer from './components/3-3.Hooks/Reducer';
 
 function App() {
   return (
     <div className="App">
-      <UncontrolledComponent />
-      <ControlledComponent />
+      <Reducer />
+      <State />
+      {/* <UncontrolledComponent /> */}
+      {/* <ControlledComponent /> */}
       {/* <List /> */}
       {/* <Condition /> */}
       {/* <Event /> */}
       {/* <ClassComponent2 /> */}
-      {/* <FunctionalComponent />
-      <ClassComponent />
-      <Composition />
-      <Extraction /> */}
+      {/* <FunctionalComponent /> */}
+      {/* <ClassComponent /> */}
+      {/* <Composition /> */}
+      {/* <Extraction /> */}
     </div>
   );
 }
---
+++ react/fc-my-app/src/components/3-3.Hooks/State.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+
+export default function State() {
+  const initialCount = 0;
+  const [count, setCount] = useState(initialCount);
+  return (
+    <div>
+      Count: {count}
+      <button onClick={() => setCount(initialCount)}>Reset</button>
+      <button onClick={() => setCount((prev) => prev - 1)}>-</button>
+      <button onClick={() => setCount((prev) => prev + 1)}>+</button>
+    </div>
+  );
+}
