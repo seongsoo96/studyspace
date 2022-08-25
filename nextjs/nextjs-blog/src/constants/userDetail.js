09dedf8915484fe98d11ed706293d8374c766334
seongsoo96 <gasmathdog@gmail.com>
2022-08-25 22:25:13 +0900

    API Routes

---
+++ nextjs/nextjs-blog/src/constants/userDetail.js
@@ -0,0 +1,6 @@
+export const userDetail = {
+  name: 'Seongsoo Lee',
+  age: 12,
+  height: '180cm',
+  weight: '65kg',
+};
---
+++ nextjs/nextjs-blog/src/pages/api/user-info/[uid].js
@@ -0,0 +1,9 @@
+export default function handler(req, res) {
+  const { uid } = req.query;
+  const cookies = req.cookies;
+  // res
+  //   .status(200)
+  //   .json({ name: `Seongsoo Lee ${uid} ${JSON.stringify(cookies)}` });
+  // res.status(404).send({ error: 'error' });
+  res.redirect(307, '/api/user');
+}
