6cac83b815339ef000dbd342d1ce2d5ef071d9c0
seongsoo96 <gasmathdog@gmail.com>
2022-09-12 18:44:20 +0900

    React to Next Migration

--- nextjs/react-next/pages/index.js
+++ nextjs/react-next/pages/index.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from '../public/logo.svg';
+import Image from 'next/image';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <Image src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -13,8 +13,7 @@ function App() {
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
-          rel="noopener noreferrer"
-        >
+          rel="noopener noreferrer">
           Learn React
         </a>
       </header>
